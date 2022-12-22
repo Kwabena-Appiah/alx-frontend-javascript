56dd4360dd136bd88de10a5939e69eedb2d4360c
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-12-22 04:46:56 +0000

    clearing blnk spces

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -12,7 +12,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         resArr.push({ status: val.status, value: val.value });
       } else {
         resArr.push({
-
           status: val.status,
           value: `Error: ${val.reason.message}`,
         });
